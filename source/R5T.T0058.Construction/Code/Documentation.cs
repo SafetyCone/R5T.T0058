68edf0149fe55c59cd6380b5943033d3436c381e
davidcoats <david@getsafetycone.com>
2021-10-01 10:59:23 -0700

    Initial commit of source-stub.

---
+++ source/R5T.T0058.Construction/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0058.Construction
+{
+    /// <summary>
+    /// Construction project for NamespacedTypeNameFilePath (strongly-typed) and other related simple strongly-typed data types..
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
---
+++ source/R5T.T0058/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0058
+{
+    /// <summary>
+    /// NamespacedTypeNameFilePath (strongly-typed) and other related simple strongly-typed data types.
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
