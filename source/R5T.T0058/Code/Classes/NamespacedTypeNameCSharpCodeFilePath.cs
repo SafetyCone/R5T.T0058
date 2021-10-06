249b7686c1b01452967aa8277b51bd27e67a499b
David Coats <david@getsafetycone.com>
2021-10-05 22:29:04 -0700

    Add NamespacedTypeNameCSharpCodeFilePath type.

---
+++ source/R5T.T0058/Code/Classes/NamespacedTypeNameCSharpCodeFilePath.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using R5T.T0049;
+using R5T.T0050;
+
+
+namespace R5T.T0058
+{
+    /// <summary>
+    /// Strongly-typed.
+    /// </summary>
+    public class NamespacedTypeNameCSharpCodeFilePath
+    {
+        public NamespacedTypeName NamespacedTypeName { get; set; }
+        public CSharpCodeFilePath CodeFilePath { get; set; }
+    }
+}
--- source/R5T.T0058/Code/Interfaces/Interface1.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-
-namespace R5T.T0058
-{
-    public interface Interface1
-    {
-    }
-}
