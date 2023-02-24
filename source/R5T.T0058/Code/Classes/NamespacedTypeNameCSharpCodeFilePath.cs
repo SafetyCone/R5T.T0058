47b66a4c670576c948ac98f1806e5e5810c38084
davidcoats <david@getsafetycone.com>
2023-02-24 00:23:02 -0800

    Interim changes.

--- source/R5T.T0058/Code/Classes/NamespacedTypeNameCSharpCodeFilePath.cs
+++ source/R5T.T0058/Code/Classes/NamespacedTypeNameCSharpCodeFilePath.cs
@@ -2,13 +2,15 @@
 
 using R5T.T0049;
 using R5T.T0050;
+using R5T.T0142;
 
 
 namespace R5T.T0058
 {
     /// <summary>
-    /// Strongly-typed.
+    /// Strongly-typed properties.
     /// </summary>
+    [DataTypeMarker]
     public class NamespacedTypeNameCSharpCodeFilePath
     {
         public NamespacedTypeName NamespacedTypeName { get; set; }
